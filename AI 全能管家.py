3b6364904472edb48ac00c3beb7a143b47e88337
qqlikeyuanshen <qqlikeyuanshen@gmail.com>
2026-04-14 13:18:38 +0800

    AI 全能管家
    原作者商丘市睢阳区文化路东方小学六年级二班的赵子晨

---
+++ AI 全能管家.py
@@ -0,0 +1,9 @@
+temp = int(input("AI  全能管家：今日温度："))
+if temp >= 30:
+    print("今天热，多喝水、注意防晒")
+elif temp >= 20:
+    print("温度舒服，认真学习")
+else: 
+    print("今天冷，多穿衣，注意保暖")
+print("AI 祝你开心每一天！")
+print("原作者商丘市睢阳区文化路东方小学六年级二班的赵子晨https://v.douyin.com/rbh-KH5hsOo/")
